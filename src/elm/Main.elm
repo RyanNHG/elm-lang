6cd61ada4561f11653eedb610db820340446c62b
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2017-11-16 02:10:23 -0600

    i am a fan of this

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Markdown
 
 
 type alias Model =
@@ -41,6 +42,43 @@ type alias SectionContent =
     { title : String
     , content : Markdown
     }
+
+
+homepageSections : List SectionContent
+homepageSections =
+    [ SectionContent "features" """
+
+### Javascript Interop
+
+Elm compiles to JavaScript, so trying out Elm is easy. Convert a small part of your app to Elm and
+[embed it in JS](/blog/how-to-use-elm-at-work) . No full rewrites, no huge time investment. More about that
+[here](http://guide.elm-lang.org/interop/).
+
+
+### No Runtime Exceptions
+
+Unlike hand-written JavaScript, Elm code does not produce runtime exceptions in practice. Instead,
+Elm uses type inference to detect problems during compilation and give [friendly hints](/blog/compilers-as-assistants)
+. This way problems never make it to your users. NoRedInk has 80k+ lines of Elm, and after more than a year
+in production, it still has not produced a single runtime exception.
+
+
+### Great Performance
+
+Elm has its own virtual DOM implementation, designed for simplicity and speed. All values are immutable in Elm, and
+[the benchmarks](/blog/blazing-fast-html-round-two) show that this helps us generate particularly fast JavaScript code.
+
+
+### Enforced Semantic Versioning
+
+Elm can detect all API changes automatically thanks to its type system. We use that information to force everything in
+[our package catalog](http://package.elm-lang.org) to follow [semantic versioning](https://github.com/elm-lang/elm-package/#version-rules)
+precisely. No more surprises in PATCH releases!
+
+    """
+    , SectionContent "examples" ""
+    , SectionContent "users" ""
+    ]
 
 
 
@@ -86,6 +124,7 @@ view model =
     div [ class "app" ]
         [ viewNavigation
         , viewHero
+        , viewSections
         ]
 
 
@@ -112,6 +151,20 @@ viewHero =
             [ h1 [ class "hero__title" ] [ text hero.title ]
             , h2 [ class "hero__subtitle" ] [ text hero.subtitle ]
             ]
+        ]
+
+
+viewSections : Html Msg
+viewSections =
+    div [ class "section__container container app__fadable app__fadable--delayed" ]
+        (List.map viewSection homepageSections)
+
+
+viewSection : SectionContent -> Html Msg
+viewSection { title, content } =
+    section [ class "section" ]
+        [ h3 [ class "section__title" ] [ text title ]
+        , Markdown.toHtml [ class "section__content rich-text" ] content
         ]
 
 
